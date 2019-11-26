815d9bbb2fd344771c87e0727bd0951aef774519
Johannes Brüderl <johannes.bruederl@eon.com>
2019-11-26 02:39:25 +0100

    fix build

--- ui/src/theme.js
+++ ui/src/theme.js
@@ -1,6 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
-import brixsans from './fonts/EON_BrixSansWeb-Regular.woff2';
 
 const theme = createMuiTheme({
  typography: {

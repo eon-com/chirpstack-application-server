1aef4d0eb290ec87b75671396dad3fcae1cf3961
Johannes Brüderl <johannes.bruederl@eon.com>
2019-11-26 02:19:43 +0100

    branding

--- ui/src/theme.js
+++ ui/src/theme.js
@@ -1,11 +1,21 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import blue from "@material-ui/core/colors/blue";
-
+import brixsans from './fonts/EON_BrixSansWeb-Regular.woff2';
 
 const theme = createMuiTheme({
+ typography: {
+    fontFamily: 'EONBrixSans',
+  },
     palette: {
-      primary: blue,
+      primary: {
+        main: "#ea1c0a",
+        light: "#ea1c0a"
+      },
+      secondary: {
+        main: "#1ea2b1"
+      },
     },
 });
+
   
 export default theme;
